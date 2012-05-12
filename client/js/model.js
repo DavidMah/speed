c337a6c05a6d5c399f87d94fa26164a7606d0d66
Alex Miller <alexmiller@gmail.com>
2012-05-11 21:52:52 -0700

    graphics for dying / win

--- client/js/model.js
+++ client/js/model.js
@@ -22,13 +22,26 @@ Game.prototype.update = function() {
 } 
 
 Game.prototype.draw = function(canvas) {
+  var context = this.canvas.getContext("2d");
+  var center = {x: 800/2 - 100/2, y: 600/2 - 30/2};
+  context.font = "15pt Helvetica";
   if (this.state == "playing") {
     this.canvas.width = this.canvas.width;
-    this.rocket.draw(this.canvas.getContext("2d"));
-    this.star.draw(this.canvas.getContext("2d"));    
+    this.rocket.draw(context);
+    this.star.draw(context);    
     for (var i = 0; i < this.planets.length; i++) {
       this.planets[i].draw(this.canvas.getContext("2d"));
     }
+  } else if (this.state == "won") {
+    context.fillStyle = "#ffff00";
+    context.fillRect(center.x, center.y, 100, 30);
+    context.fillStyle = "#000000";
+    context.fillText("Nice!", center.x + 5, center.y + 25);
+  } else if (this.state == "dead") {
+    context.fillStyle = "#ff0000";
+    context.fillRect(center.x, center.y, 100, 30);
+    context.fillStyle = "#000000";
+    context.fillText("You Died.", center.x + 5, center.y + 25);
   }
 }
 
