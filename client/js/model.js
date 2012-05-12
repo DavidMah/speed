b55db0764763957daf7fc2e2ff0095e69cf9ab0b
Alex Miller <alexmiller@gmail.com>
2012-05-11 18:40:25 -0700

    planet collision

--- client/js/model.js
+++ client/js/model.js
@@ -4,24 +4,28 @@
  
 function Game(canvas) {
   this.canvas = canvas;
-  this.mode = "waiting";
+  this.state = "waiting";
   this.time = 0;
   this.planets = [];
-  this.rocket;
   this.rocket = new Rocket();
   this.startX = 800 / 2;
   this.startY = 800 - 20;
 }
 
 Game.prototype.update = function() {
-  this.rocket.update();
+  if (this.state == "playing") {
+    this.rocket.update();
+    this.checkCollisions();
+  }
 } 
 
 Game.prototype.draw = function(canvas) {
-  this.canvas.width = this.canvas.width;
-  this.rocket.draw(this.canvas.getContext("2d"));
-  for (var i = 0; i < this.planets.length; i++) {
-    this.planets[i].draw(this.canvas.getContext("2d"));
+  if (this.state == "playing") {
+    this.canvas.width = this.canvas.width;
+    this.rocket.draw(this.canvas.getContext("2d"));
+    for (var i = 0; i < this.planets.length; i++) {
+      this.planets[i].draw(this.canvas.getContext("2d"));
+    }
   }
 }
 
@@ -31,13 +35,31 @@ Game.prototype.newGame = function(planets) {
   this.rocket.vx = 0;
   this.rocket.vy = 0;
   for (var i = 0; i < planets.length; i++) {
+    this.newPlanet(planets[i].x, planets[i].y, planets[i].radius);
+  }
+  this.state = "playing";
+}
 
+Game.prototype.checkCollisions = function() {
+  for (var i = 0; i < this.planets.length; i++) {
+    var p = this.planets[i];
+    var r = this.rocket;
+    var dx = p.x - r.x;
+    var dy = p.y - r.y;
+    var dist = Math.sqrt(dx * dx + dy * dy);
+    if (dist < p.radius + r.radius) {
+      this.die();
+    }
   }
 }
 
 Game.prototype.newPlanet = function(x, y, radius) {
   var planet = new Planet(x, y, radius);
-  planets.push(planet);
+  this.planets.push(planet);
+}
+
+Game.prototype.die = function() {
+  this.state = "dead";
 }
 
 /**
@@ -66,14 +88,13 @@ Rocket.prototype.update = function() {
   if (KEYBOARD[DOWN]) {
     this.vy += this.acceleration;
   }
-  
   this.x += this.vx;
   this.y += this.vy;
 }
 
 Rocket.prototype.draw = function(ctx) {
   ctx.fillStyle = "#FFFFFF";  
-  ctx.fillRect(this.x, this.y, 15, 15);
+  ctx.fillRect(this.x - 7, this.y - 7, 15, 15);
 }
 
 /**
@@ -92,5 +113,6 @@ Planet.prototype.update = function() {
 
 Planet.prototype.draw = function(ctx) {
   ctx.fillStyle = "#FFFFFF";  
-  ctx.arc(this.x, this.y, this.radius, 0, Math.PI*2, true); 
+  ctx.arc(this.x, this.y, this.radius, 0 , 2 * Math.PI, false);
+  ctx.fill();
 }
